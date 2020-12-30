034ae5bb4c81d4cc7ea94c229c4086615d7271fc
kuriyantoadi <melatikuning212@gmail.com>
2020-12-30 20:05:51 +0700

    update 30 des 2020, update 2

--- phpmyadmin/phpmyadmin-lock.sh
+++ phpmyadmin/phpmyadmin-lock.sh
@@ -11,14 +11,14 @@ if [ 1 = $pilih ]
 then
   sudo rm /opt/lampp/etc/extra/httpd-xampp.conf
   sudo cp httpd-xampp.conf-lock /opt/lampp/etc/extra/
-  sudo /opt/lampp/etc/extra/httpd-xampp.conf-lock /opt/lampp/etc/extra/httpd-xampp.conf
+  sudo mv /opt/lampp/etc/extra/httpd-xampp.conf-lock /opt/lampp/etc/extra/httpd-xampp.conf
   echo "phpmyadmin lock - berhasil"
   exit 0
 elif [ 2 = $pilih ]
 then
   sudo rm /opt/lampp/etc/extra/httpd-xampp.conf
   sudo cp httpd-xampp.conf-unlcok /opt/lampp/etc/extra/
-  sudo /opt/lampp/etc/extra/httpd-xampp.conf-unlock /opt/lampp/etc/extra/httpd-xampp.conf
+  sudo mv /opt/lampp/etc/extra/httpd-xampp.conf-unlock /opt/lampp/etc/extra/httpd-xampp.conf
   echo "phpmyadmin unlock - berhasil"
   exit 0
 else
